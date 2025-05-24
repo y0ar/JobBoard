28b7dcd6d11ee5cc992ce78c4ae23fcb547ba6c8
y0ar <youssef-801@hotmail.com>
2025-05-24 20:46:51 +0100

    Client: Initial commit

--- frontend/client/src/api/categories.ts
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-import { Category } from "../types";
-
-export const getCategories = async (): Promise<Category[]> => {
-  const res = await api.get<Category[]>("/Categories");
-  return res.data;
-};
--- frontend/client/src/api/companies.ts
+++
@@ -1,7 +0,0 @@
-import api from "./axios";
-import { Company } from "../types";
-
-export const getCompany = async (): Promise<Company[]> => {
-  const res = await api.get<Company[]>("/Company");
-  return res.data;
-};
